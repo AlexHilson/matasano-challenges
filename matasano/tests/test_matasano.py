e4cec0a055a506450e0c6cdfed2d54eae48cde8a
AlexHilson <alexhilsonwork@googlemail.com>
2015-05-17 13:49:34 +0100

    Failing unit test for Set 1 Challenge 1

---
+++ matasano/tests/test_matasano.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import unittest
+import matasano
+
+class hex_to_base64(unittest.TestCase):
+
+    def test_convert(self):
+        test_in = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+        test_out = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+        self.assertEqual(matasano.hex2base64(test_in), test_out)
